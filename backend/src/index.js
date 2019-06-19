76734c6d8dde074229ac7a0f369589689711f1d4
DavidNatanael <davidstinghen@gmail.com>
2019-06-19 13:33:53 -0300

    fet: add path to static files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
+// conexão com o banco de dados
 mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0-weljl.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 
+// caminho para arquivos estáticos
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+// chamada as rotas
 app.use(require('./routes'))
+// declarando que o server deve ouvir a porta 3333
 app.listen(3333)
