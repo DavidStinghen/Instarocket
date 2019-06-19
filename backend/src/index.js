72efdf5fce315a61c63d6943db1030ea50502f4b
DavidNatanael <davidstinghen@gmail.com>
2019-06-19 15:45:03 -0300

    fet: use websocket & cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
+
+// tornando disponiveis requisições http e também websocket
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 // conexão com o banco de dados
 mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0-weljl.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 
+// tornando utilizavel o websocket para toda a aplicação
+app.use((req, res, next) => {
+    req.io = io
+    next()
+})
+// liberado acesso a api
+app.use(cors())
 // caminho para arquivos estáticos
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 // chamada as rotas
 app.use(require('./routes'))
 // declarando que o server deve ouvir a porta 3333
-app.listen(3333)
+server.listen(3333)
