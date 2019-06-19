31f75491dd6e96aac40645caa7268f3eb8552ff9
DavidNatanael <davidstinghen@gmail.com>
2019-06-19 12:23:51 -0300

    fet: use multer in route /posts for store

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const routes = new express.Router()
+
 const PostController = require('./controllers/PostController')
 
-routes.post('./posts', PostController.store)
+const uploadConfig = require('./config/upload')
+
+// uso do multer para ser possivel o envio de arquivos multipart
+const multer = require('multer')
+const upload = multer(uploadConfig)
+
+/**
+ * rota para adicionar posts
+ * declarando que sera feito upload de uma unica imagem
+ * chamada a função store
+ */
+routes.post('/posts', upload.single('image'), PostController.store)
 
 module.exports = routes
