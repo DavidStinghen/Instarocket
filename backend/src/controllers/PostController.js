d77459a8f87a98cf241570df03265019d2f9e236
DavidNatanael <davidstinghen@gmail.com>
2019-06-19 13:34:51 -0300

    fet: add method to resize, change ext of the file, delete original file

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,4 +1,7 @@
 const Post = require('../models/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = {
 
@@ -8,17 +11,32 @@ module.exports = {
         return res.json(posts)
     },
 
-    //metodo para adicionar novos posts com imagens
+    /** 
+     * metodo para adicionar novos posts com imagens
+     * definindo o nome da imagem que aparece na url como nome.jpg
+     * usado sharp para redimensionar a imagem
+     * usado fs para deletar a imagem de tamanho original
+    */
     async store (req, res) {
         const { author, place, description, hashtags } = req.body
         const { filename: image } = req.file
+
+        const [name] =  image.split('.')
+        const filename = `${name}.jpg`
+
+        await sharp( req.file.path)
+            .resize(500)
+            .jpeg({ quality: 70 })
+            .toFile(path.resolve(req.file.destination, 'resized', filename))
+
+        fs.unlinkSync(req.file.path)
 
         const posts = await Post.create({
             author,
             place,
             description,
             hashtags,
-            image
+            image: filename
         })
         return res.json(posts)
     },
