d65d82a7cd5b04cfdfb1666f173a901bbf4a5116
DavidNatanael <davidstinghen@gmail.com>
2019-06-19 12:46:50 -0300

    fet: add route to like posts

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const routes = new express.Router()
 
 const PostController = require('./controllers/PostController')
+const LikeController = require('./controllers/LikeController')
 
 const uploadConfig = require('./config/upload')
-
 // uso do multer para ser possivel o envio de arquivos multipart
 const multer = require('multer')
 const upload = multer(uploadConfig)
@@ -15,8 +15,10 @@ const upload = multer(uploadConfig)
  * chamada a função store
  */
 routes.post('/posts', upload.single('image'), PostController.store)
-
 // rota para listar os posts
 routes.get('/posts', PostController.index)
+
+// rota para dar like no post
+routes.post('/posts/:id/like', LikeController.store)
 
 module.exports = routes
