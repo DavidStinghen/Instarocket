850f696f2f997cdb0bdf783100cbe0c4e5239f67
DavidNatanael <davidstinghen@gmail.com>
2019-06-21 10:42:18 -0300

    fet: corrected controllers and models

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -18,32 +18,34 @@ module.exports = {
      * usado fs para deletar a imagem de tamanho original
      * usado io para enviar uma mensagem para o frontend quando for feita um post
     */
-    async store (req, res) {
-        const { author, place, description, hashtags } = req.body
-        const { filename: image } = req.file
+   async store(req, res){
+        const { author, place, description, hashtags } = req.body;
+        const { filename: image } = req.file;
 
-        const [name] =  image.split('.')
-        const filename = `${name}.jpg`
+        const [name] = image.split('.');
+        const fileName = `${name}.jpg`;
 
-        await sharp( req.file.path)
+        await sharp(req.file.path)
             .resize(500)
-            .jpeg({ quality: 70 })
-            .toFile(path.resolve(req.file.destination, 'resized', filename))
+            .jpeg( {quality: 70})
+            .toFile(
+                path.resolve(req.file.destination, 'resized', fileName)
+            )
 
-        fs.unlinkSync(req.file.path)
+        fs.unlinkSync(req.file.path);
 
         const post = await Post.create({
             author,
             place,
             description,
             hashtags,
-            image: filename
-        })
+            image: fileName
+        });
 
-        req.io,emit('post', post)
+        req.io.emit('post', post);
 
-        return res.json(post)
-    },
+        return res.json(post);
+    }
 
     
 }
